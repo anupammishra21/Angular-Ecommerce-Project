9321b2be8e152c48f1e280f4120388ff3963a6a2
Anupam <anupammishr678@gmail.com>
2023-01-30 21:30:08 +0530

    lense changed

--- src/app/components/lence/lence.component.ts
+++ src/app/components/lence/lence.component.ts
@@ -9,26 +9,63 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./lence.component.scss'],
 })
 export class LenceComponent implements OnInit {
-  lence:any
+  lence: any;
   product: any | undefined;
   productID!: string | null;
   prodnew_id!: any;
   productList: any;
-  constructor(private ser: ProductService,private cart:CartServiceService) {}
+  constructor(private ser: ProductService, private cart: CartServiceService) {}
   prod!: any;
+  sortType = 'asc';
+  sortType2 = 'desc';
 
-  
+  sortOptions = [
+    {
+      title: 'Price- Low to High',
+      asc: true,
+    },
+    {
+      title: 'Price- High to Low',
+      asc: false,
+    },
+  ];
+  selectedSortOption: any;
+
   ngOnInit(): void {
-    
-    this.lence=this.ser.product2
+    // this.lence=this.ser.product2
+    this.prod = this.formatData(this.ser.product2);
+    console.log(this.prod);
+    this.prod.forEach((a: any) => {
+      Object.assign(a, { quantity: 1, total: a.prod_price });
+    });
+    this.selectedSortOption = this.sortOptions[0];
+    this.prod = this.sortData(this.prod, this.selectedSortOption.asc);
+  }
 
-    this.lence.forEach((a:any) => {
-      Object.assign(a,{quantity:1,total:a.prod_price})
+  changeSort(option: any) {
+    this.selectedSortOption = option;
+    this.prod = this.sortData(this.prod, this.selectedSortOption.asc);
+  }
+
+  sortData(data: any, asc: boolean) {
+    return data.sort((a: any, b: any) =>
+      asc ? a.price - b.price : b.price - a.price
+    );
+  }
+
+  formatData(data: any) {
+    return data.map((e: any) => {
+      return {
+        ...e,
+        price: e.prod_price,
+        //.replace(/,/g, ''),
+        // removing , from price
+      };
     });
   }
 
-  addToCart(data:any){
-    this.cart.addToCart(data)
+  addToCart(data: any) {
+    this.cart.addToCart(data);
   }
 
   // formatData(data: any) {
@@ -42,27 +79,18 @@ export class LenceComponent implements OnInit {
   //   });
   // }
 
+  onSearchTextEntered(searchValue: string) {
+    const allProducts = this.sortData(
+      this.formatData(this.ser.product2),
+      this.selectedSortOption.asc
+    );
 
-  onSearchTextEntered(searchValue: string){
-    // const allProducts = this.formatData(this.lence.product);
-    // if (searchValue) {
-    //   this.prod= allProducts.filter((data:any)=>{
-    //     return data.prod_name.toLowerCase().includes(searchValue)
-    //   })
-    //   console.log(this.prod);
-      
-    // }
-    // else {
-    //   this.prod = allProducts;
-    // }
     if (searchValue) {
-      this.lence = this.productList.filter((data: any) => {
+      this.prod = allProducts.filter((data: any) => {
         return data.prod_name.toLowerCase().includes(searchValue);
       });
     } else {
-      this.productList = this.prodnew_id;
+      this.prod = allProducts;
     }
   }
-
-  }
-
+}
